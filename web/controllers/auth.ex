fe1e6948d0b681d7fe762f16328b7c53cd7717d8
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-10-07 15:17:55 +0200

    podcast frontend show

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -12,7 +12,7 @@ defmodule Pan.Auth do
   end
 
   def login(conn, user) do
-    conn 
+    conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
@@ -48,7 +48,7 @@ defmodule Pan.Auth do
     else
       conn
       |> put_flash(:error, "You must be logged in to access that page.")
-      |> redirect(to: Helpers.page_path(conn, :index))
+      |> redirect(to: Helpers.podcast_frontend_path(conn, :index))
       |> halt()
     end
   end
@@ -60,7 +60,7 @@ defmodule Pan.Auth do
     else
       conn
       |> put_flash(:error, "You must be admin to access this page.")
-      |> redirect(to: Helpers.page_path(conn, :index))
+      |> redirect(to: Helpers.podcast_frontend_path(conn, :index))
       |> halt()
     end
   end
--- web/controllers/podcast_frontend_controller.ex
+++ web/controllers/podcast_frontend_controller.ex
@@ -2,6 +2,7 @@ defmodule Pan.PodcastFrontendController do
   use Pan.Web, :controller
 
   alias Pan.Podcast
+  alias Pan.Episode
 
   def index(conn, _params) do
     podcasts = Repo.all(Podcast)
@@ -10,6 +11,8 @@ defmodule Pan.PodcastFrontendController do
 
   def show(conn, %{"id" => id}) do
     podcast = Repo.get!(Podcast, id)
+    podcast = Repo.preload(podcast, [:language, :owner])
+    podcast = Repo.preload(podcast, episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
     render(conn, "show.html", podcast: podcast)
   end
 end
