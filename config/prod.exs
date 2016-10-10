2eae822129dc391b646ee55b9db218ab69e19fe2
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-10-10 10:09:51 +0200

    lets encrypt config

--- config/prod.exs
+++ config/prod.exs
@@ -1,39 +1,17 @@
 use Mix.Config
 
 config :pan, Pan.Endpoint,
-  http: [port: 8888],
-  url: [host: "alpha.panoptikum.io", port: 80],
+  https: [port: 8888,
+          keyfile: "/etc/letsencrypt/live/alpha.panoptikum.io/privkey.pem",
+          certfile: "/etc/letsencrypt/live/alpha.panoptikum.io/cert.pem"],
+  url: [host: "alpha.panoptikum.io", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   root: ".",
-  version: Mix.Project.config[:version]
+  version: Mix.Project.config[:version],
+  force_ssl: [hsts: true]
 
 config :logger, level: :info
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :pan, Pan.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :pan, Pan.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
 config :phoenix, :serve_endpoints, true
 
 import_config "prod.secret.exs"
