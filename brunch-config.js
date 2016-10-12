2453dd4aec11307fec114003659ffb9b905f28cc
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-10-12 11:42:03 +0200

    navbar

--- brunch-config.js
+++ brunch-config.js
@@ -54,6 +54,10 @@ exports.config = {
 
   npm: {
     enabled: true,
-    whitelist: ["phoenix", "phoenix_html"]
+    whitelist: ["phoenix", "phoenix_html", "jquery"],
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery'
+    }
   }
 };
