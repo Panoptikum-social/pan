ecf8ed212a9af8d2e7188562aabd5a607475063d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-10-26 10:46:18 +0200

    not compiling...

--- lib/pan/parser/persistor.ex
+++ lib/pan/parser/persistor.ex
@@ -1,13 +1,12 @@
 defmodule Pan.Parser.Persistor do
   use Pan.Web, :controller
-  alias Pan.User
 
   def call(map) do
     podcast_map = Map.drop(map, [:episodes, :feed, :contributors])
     feed_map =    Map.drop(map[:feed], [:alternate_feeds])
     alternate_feeds_map = map[:feed][:alternate_feeds]
 
-map[:contributors]
+    map[:contributors]
 
 #    {:ok ,owner }  = find_or_create_owner(map[:owner])
 #    {:ok, podcast} = find_or_create_podcast(map[:podcast], owner.id)
@@ -18,8 +17,8 @@ map[:contributors]
 
 
   def find_or_create_owner(owner_map) do
-    case Repo.get_by(User, email: owner_map[:email]) do
-      nil -> %User{}
+    case Repo.get_by(Pan.User, email: owner_map[:email]) do
+      nil -> %Pan.User{}
              |> Map.merge(owner_map)
              |> Map.merge(%{username: owner_map[:name]})
              |> Repo.insert()
@@ -29,8 +28,8 @@ map[:contributors]
 
 
   def find_or_create_podcast(podcast_map, owner_id) do
-    case Repo.get_by(Podcast, title: podcast_map[:title]) do
-      nil -> %Podcast{owner_id: owner_id}
+    case Repo.get_by(Pan.Podcast, title: podcast_map[:title]) do
+      nil -> %Pan.Podcast{owner_id: owner_id}
              |> Map.merge(podcast_map)
              |> Repo.insert()
       podcast -> {:ok, podcast}
@@ -39,8 +38,8 @@ map[:contributors]
 
 
   def find_or_create_feed(feed_map, podcast_id) do
-    case Repo.get_by(Feed, self_link_url: feed_map[:self_link_url]) do
-      nil -> %Feed{podcast_id: podcast_id}
+    case Repo.get_by(Pan.Feed, self_link_url: feed_map[:self_link_url]) do
+      nil -> %Pan.Feed{podcast_id: podcast_id}
              |> Map.merge(feed_map)
              |> Repo.insert()
       feed -> {:ok, feed}
@@ -50,8 +49,8 @@ map[:contributors]
 
   def find_or_create_alternate_feeds(alternate_feeds_map, feed_id) do
     for {uuid, alternate_feed_map} <- alternate_feeds_map do
-      case Repo.get_by(AlternateFeed, url: alternate_feed_map[:url])
-        nil -> %AlternateFeed(feed_id: feed_id)
+      case Repo.get_by(Pan.AlternateFeed, url: alternate_feed_map[:url])
+        nil -> %Pan.AlternateFeed(feed_id: feed_id)
                |> Map.merge(alternate_feed_map)
                |> Repo.insert()
         alternate_feed -> {:ok, alternate_feed}
@@ -60,9 +59,9 @@ map[:contributors]
   end
 
 
-  def find_or_create_contributor() do
-    case Repo.get_by(Contributor, uri: contributor_map[:uri]) do
-      nil -> %Contributor()
+  def find_or_create_contributor(contributor_map) do
+    case Repo.get_by(Pan.Contributor, uri: contributor_map[:uri]) do
+      nil -> %Pan.Contributor()
              |> Map.merge(contributor_map)
              |> Repo.insert()
       contributor -> {:ok, contributor}
