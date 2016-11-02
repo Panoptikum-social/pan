fab25204617a70cd11288758a2b6aefeaf7209d4
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-11-02 21:28:07 +0100

    DRY up podcast preloading

--- web/controllers/podcast_frontend_controller.ex
+++ web/controllers/podcast_frontend_controller.ex
@@ -13,25 +13,19 @@ defmodule Pan.PodcastFrontendController do
 
 
   def show(conn, %{"id" => id}) do
-    podcast = Repo.get!(Podcast, id)
-    |> Repo.preload([:languages, :owner, :feeds, :followers])
-    |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
+    podcast = get_with_relations id
     render(conn, "show.html", podcast: podcast)
   end
 
   def like(conn, %{"id" => id}) do
     if conn.assigns.current_user do
-      podcast = Repo.get!(Podcast, id)
-      |> Repo.preload([:languages, :owner, :feeds, :followers])
-      |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
+      podcast = get_with_relations id
       podcast
       |> Ecto.Changeset.change
       |> Ecto.Changeset.put_assoc(:followers, [conn.assigns.current_user | podcast.followers])
       |> Repo.update!
 
-      podcast = Repo.get!(Podcast, id)
-      |> Repo.preload([:languages, :owner, :feeds, :followers])
-      |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
+      podcast = get_with_relations id
       conn
       |> render("show.html", podcast: podcast)
     else
@@ -40,18 +34,14 @@ defmodule Pan.PodcastFrontendController do
 
   def unlike(conn, %{"id" => id}) do
     if conn.assigns.current_user do
-      podcast = Repo.get!(Podcast, id)
-      |> Repo.preload([:languages, :owner, :feeds, :followers])
-      |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
+      podcast = get_with_relations id
 
       podcast
       |> Ecto.Changeset.change
       |> Ecto.Changeset.put_assoc(:followers, List.delete(podcast.followers, conn.assigns.current_user))
       |> Repo.update!
 
-      podcast = Repo.get!(Podcast, id)
-      |> Repo.preload([:languages, :owner, :feeds, :followers])
-      |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
+      podcast = get_with_relations id
       conn
       |> render("show.html", podcast: podcast)
     else
@@ -62,5 +52,11 @@ defmodule Pan.PodcastFrontendController do
     podcast = Repo.get!(Podcast, id)
     podcast = Repo.preload(podcast, :feeds)
     render(conn, "subscribe_button.html", podcast: podcast)
+  end
+
+  defp get_with_relations(id) do
+    Repo.get!(Podcast, id)
+    |> Repo.preload([:languages, :owner, :feeds, :followers])
+    |> Repo.preload(episodes: from(episode in Episode, order_by: [desc: episode.publishing_date]))
   end
 end
