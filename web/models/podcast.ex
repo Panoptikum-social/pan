c95af3b39a3b900e2451ebcbe303e94d63cb3ae7
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-11-30 09:38:00 +0100

    bug in follow relation (still old db schema there...)

--- web/models/podcast.ex
+++ web/models/podcast.ex
@@ -28,9 +28,10 @@ defmodule Pan.Podcast do
     many_to_many :categories, Pan.Category, join_through: "categories_podcasts", on_delete: :delete_all
     many_to_many :contributors, Pan.Contributor, join_through: "contributors_podcasts", on_delete: :delete_all
     many_to_many :listeners, Pan.User, join_through: "subscriptions", on_delete: :delete_all
-    many_to_many :followers, Pan.User, join_through: "followers_podcasts", on_delete: :delete_all, on_replace: :delete
+    many_to_many :followers, Pan.User, join_through: "likes", join_keys: [podcast_id: :id, enjoyer_id: :id]
     many_to_many :languages, Pan.Language, join_through: "languages_podcasts", on_delete: :delete_all
   end
+
 
   @required_fields ~w(title website description summary image_title image_url
                       last_build_date payment_link_title payment_link_url author explicit
