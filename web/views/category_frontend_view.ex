e84b57e0ffb33aa52f302630bf3d67b189b4ba8a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-12-01 15:53:54 +0100

    categories in columns

--- web/views/category_frontend_view.ex
+++ web/views/category_frontend_view.ex
@@ -11,13 +11,13 @@ defmodule Pan.CategoryFrontendView do
 
 
   def btn_cycle(counter) do
-    Enum.at(["btn-default", "btn-info", "btn-danger",
+    Enum.at(["btn-normal", "btn-info", "btn-danger",
              "btn-warning", "btn-primary", "btn-success"], rem(counter, 6))
   end
 
 
   def color_cycle(counter) do
-    Enum.at(["666", "fff", "fff",
+    Enum.at(["fff", "fff", "fff",
              "fff", "fff", "fff"], rem(counter, 6))
   end
 
