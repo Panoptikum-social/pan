ef71598cee3b030b8920c05cfe2a8856da665506
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-01-18 14:03:15 +0100

    namespacing bug

--- lib/pan/parser/owner.ex
+++ lib/pan/parser/owner.ex
@@ -6,8 +6,8 @@ defmodule Pan.Parser.Owner do
     {:ok, owner} = Persona.get_or_insert(owner_map)
 
     %Pan.Engagement{persona_id: owner.id,
-                podcast_id: podcast_id,
-                role: "owner"}
+                    podcast_id: podcast_id,
+                    role: "owner"}
     |> Repo.insert()
   end
 end
