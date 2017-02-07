64c947eb7e0946712943579a41d801e5f2bd3ffe
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-02-07 11:05:24 +0100

    extend search to personas and users

--- web/controllers/search_frontend_controller.ex
+++ web/controllers/search_frontend_controller.ex
@@ -3,11 +3,20 @@ defmodule Pan.SearchFrontendController do
   alias Pan.Podcast
   alias Pan.Episode
   alias Pan.Category
+  alias Pan.User
+  alias Pan.Persona
 
   def new(conn, params) do
     sqlfrag = "%" <> params["search"]["searchstring"] <> "%"
 
     page = if params["page"] != nil, do: String.to_integer(params["page"]), else: 1
+
+    users = Repo.all(from u in User, where: ilike(u.name, ^sqlfrag) or
+                                            ilike(u.username, ^sqlfrag))
+
+    personas = Repo.all(from p in Persona, where: ilike(p.pid, ^sqlfrag) or
+                                                  ilike(p.name, ^sqlfrag) or
+                                                  ilike(p.uri, ^sqlfrag))
 
     categories = Repo.all(from c in Category, where: ilike(c.title, ^sqlfrag))
 
@@ -27,9 +36,10 @@ defmodule Pan.SearchFrontendController do
                |> Repo.paginate(page: page, page_size: 10)
 
     render(conn, "new.html", searchstring: params["search"]["searchstring"],
+                             users: users,
+                             personas: personas,
                              categories: categories,
                              podcasts: podcasts,
                              episodes: episodes)
   end
 end
-
