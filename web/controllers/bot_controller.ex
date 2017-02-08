e7f0ba5e3346fe83e515430f91c60e90b35562e8
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2017-02-08 20:56:10 +0100

    Respond with generic template of podcasts

--- web/controllers/bot_controller.ex
+++ web/controllers/bot_controller.ex
@@ -1,5 +1,6 @@
 defmodule Pan.BotController do
   use Pan.Web, :controller
+  alias Pan.Podcast
 
   # To test using ngrok, call:
   #   $ ngrok http 4000
@@ -12,39 +13,24 @@ defmodule Pan.BotController do
 
   def message(conn, %{"entry" => [%{"messaging" => [%{"message" => %{"text" => message}, "sender" => %{"id" => sender_id}}]}]}) do
     sqlfrag = "%" <> message <> "%"
-    podcast_ids = from(p in Pan.Podcast, where: ilike(p.title, ^sqlfrag),
-                                      select: p.id)
-                |> Repo.all()
-
-    users_also_liking = from(l in Pan.Like, where: l.podcast_id in ^podcast_ids and
-                                               is_nil(l.chapter_id) and
-                                               is_nil(l.episode_id),
-                                        select: l.enjoyer_id)
-                        |> Repo.all()
-                        |> Enum.uniq
-    also_liked = from(l in Pan.Like, join: p in assoc(l, :podcast),
-                                 where: l.enjoyer_id in ^users_also_liking and
-                                        is_nil(l.chapter_id) and
-                                        is_nil(l.episode_id) and
-                                        not(l.podcast_id in ^podcast_ids),
-                                 group_by: p.id,
-                                 select: [count(l.podcast_id), p.id, p.title],
-                                 order_by: [desc: count(l.podcast_id)],
-                                 limit: 10)
-                 |> Repo.all()
-    reply = case also_liked do
-      [[_, _, title] | _] ->
-        title
-      [] ->
-        "No matches found"
-    end
-
+    podcasts = from(p in Podcast, where: ilike(p.title,       ^sqlfrag) or
+                                         ilike(p.description, ^sqlfrag) or
+                                         ilike(p.summary,     ^sqlfrag) or
+                                         ilike(p.author,      ^sqlfrag),
+                                  limit: 5)
+                  |> Repo.all()
     data = %{
       recipient: %{
         id: sender_id
       },
       message: %{
-        text: reply
+        attachment: %{
+          type: "template",
+          payload: %{
+            template_type: "generic",
+            elements: Enum.map(podcasts, &(podcast_json(conn, &1)))
+          }
+        }
       }
     }
     |> Poison.encode!
@@ -53,6 +39,15 @@ defmodule Pan.BotController do
       access_token: Application.get_env(:pan, :bot)[:fb_access_token]
     }
     |> URI.encode_query()
+
+    body = %{
+      setting_type: "domain_whitelisting",
+      whitelisted_domains: ["https://20ba4076.ngrok.io"],
+      domain_action_type: "add"
+    }
+    |> Poison.encode!
+    "https://graph.facebook.com/v2.6/me/thread_settings?#{params}"
+    |> HTTPotion.post([body: body,  headers: ["Content-Type": "application/json"]])
 
     "https://graph.facebook.com/v2.6/me/messages?#{params}"
     |> HTTPotion.post([body: data,  headers: ["Content-Type": "application/json"]])
@@ -66,5 +61,32 @@ defmodule Pan.BotController do
     conn
     |> put_status(200)
     |> text("ok")
+  end
+
+  defp podcast_json(conn, podcast) do
+    %{
+      title: podcast.title,
+      image_url: podcast.image_url,
+      subtitle: podcast.description,
+      default_action: %{
+        type: "web_url",
+        url: "https://20ba4076.ngrok.io" <> podcast_frontend_path(conn, :show, podcast),
+        messenger_extensions: true,
+        webview_height_ratio: "tall",
+        fallback_url: "https://20ba4076.ngrok.io" <> podcast_frontend_path(conn, :show, podcast)
+      },
+      buttons: [
+        %{
+          type: "web_url",
+          url: "https://20ba4076.ngrok.io" <> podcast_frontend_path(conn, :show, podcast),
+          title: "In Panoptikum"
+        },
+        %{
+          type: "web_url",
+          url: "https://20ba4076.ngrok.io" <> podcast_frontend_path(conn, :show, podcast),
+          title: "View Website"
+        }
+      ]
+    }
   end
 end
