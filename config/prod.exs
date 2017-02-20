e22ff27138a27be636976bb4e251fdb0b1bdc584
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-02-20 10:56:41 +0100

    back to ssl

--- config/prod.exs
+++ config/prod.exs
@@ -1,13 +1,10 @@
 use Mix.Config
 
 config :pan, Pan.Endpoint,
-  http: [port: 8888],
-  url: [host: "beta.panoptikum.io"],
-
-#  https: [port: 8888,
-#          keyfile: "/etc/letsencrypt/live/beta.panoptikum.io/privkey.pem",
-#          certfile: "/etc/letsencrypt/live/beta.panoptikum.io/cert.pem"],
-#  url: [host: "beta.panoptikum.io", port: 443],
+  https: [port: 8888,
+          keyfile: "/etc/letsencrypt/live/beta.panoptikum.io/privkey.pem",
+          certfile: "/etc/letsencrypt/live/beta.panoptikum.io/cert.pem"],
+  url: [host: "beta.panoptikum.io", port: 443],
 
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
