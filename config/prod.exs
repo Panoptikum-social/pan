bf8dec3bcf2343b8838178194da109fbffb5f7fb
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-03-30 11:45:10 +0200

    job for index update

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,8 @@ config :phoenix, :serve_endpoints, true
 
 config :timelier, crontab: [{{[42],:any,:any,:any,:any},
                             {Pan.Podcast,:import_stale_podcasts, []}}]
+config :timelier, crontab: [{{[48],:any,:any,:any,:any},
+                            {Pan.Search,:push, [1]}}]
 
 config :pan, :environment, "prod"
 
