2d91aed9588cbb401d11b05c18e5cad0a958eee7
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-06-13 12:27:49 +0200

    liked podcasts

--- web/controllers/page_frontend_controller.ex
+++ web/controllers/page_frontend_controller.ex
@@ -2,6 +2,7 @@ defmodule Pan.PageFrontendController do
   use Pan.Web, :controller
 
   alias Pan.Subscription
+  alias Pan.Like
 
   def home(conn, _params) do
     popular_podcasts = ConCache.get_or_store(:slow_cache, :popular_podcasts, fn() ->
@@ -13,6 +14,16 @@ defmodule Pan.PageFrontendController do
                          |> Repo.all()
                        end)
 
-    render(conn, "home.html", popular_podcasts: popular_podcasts)
+    liked_podcasts = ConCache.get_or_store(:slow_cache, :liked_podcasts, fn() ->
+                         (from l in Like, join: p in assoc(l, :podcast),
+                                          group_by: p.id,
+                                          select: [count(l.podcast_id), p.id, p.title],
+                                          order_by: [desc: count(l.podcast_id)],
+                                          limit: 5)
+                         |> Repo.all()
+                       end)
+
+    render(conn, "home.html", popular_podcasts: popular_podcasts,
+                              liked_podcasts: liked_podcasts)
   end
 end
