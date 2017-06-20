533e2d4273524a9a6946ca32137427a5cf159a46
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-06-20 10:05:57 +0200

    parser

--- lib/pan/parser/analyzer.ex
+++ lib/pan/parser/analyzer.ex
@@ -196,7 +196,8 @@ defmodule Pan.Parser.Analyzer do
     :"cba:duration", :"cba:attachmentID", :"im:image", :"episode_mp3", :"jwplayer:talkId",
     :"aidsgov:transcript", :"foto_428", :"podcast:brandStory", :"thr:in-reply-to", :"media:hash",
     :"posterous:author", :"companyLogo", :"coverimage", :"media:thumb", :"podcast:spotlight",
-    :"itunes:name", :"itunes:episode", :"itunes:episodeType", :"itunes:season", :"georss:elev"
+    :"itunes:name", :"itunes:episode", :"itunes:episodeType", :"itunes:season", :"georss:elev",
+    :"podcast:category"
   ], do: %{}
 
 
