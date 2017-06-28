018a30108bd48e8e3045e05f68b6727017cc16b3
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-06-28 14:56:49 +0200

    parser, admin navbar

--- lib/pan/parser/analyzer.ex
+++ lib/pan/parser/analyzer.ex
@@ -180,7 +180,7 @@ defmodule Pan.Parser.Analyzer do
     :"blip:item_id", :"blip:item_type", :"blip:rating", :"blip:datestamp", :"blip:language", :tags,
     :"blip:adChannel", :"blip:categories", :"blip:license", :"blip:puredescription", :"dc:rights",
     :"blip:thumbnail_src", :"blip:", :"blip:embedUrl", :"blip:embedLookup", :"blip:runtime", :draft,
-    :"blip:adminRating", :"blip:core_value", :"blip:core", :"blip:recommendable", :"avms:id",
+    :"blip:adminRating", :"blip:core_value", :"blip:core", :"blip:recommendable", :"avms:id", :hq,
     :"blip:recommendations", :"yv:adInfo", :"blip:smallThumbnail", :"clipper:id", :"a10:link",
     :"uzhfeeds:image", :"amp:banner", :"itunes:isClosedCaptioned", :"blip:poster_image", :showThumb,
     :"georss:where", :"itunes:subitle", :"media:category", :"geourl:latitude", :"geourl:longitude",
