ee41cacfebbd6159e4c4311f96edb447d12fe230
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-08-29 15:40:47 +0200

    try to switch to :gen_event

--- lib/logger/backends/exception_notification.ex
+++ lib/logger/backends/exception_notification.ex
@@ -1,5 +1,13 @@
 defmodule Logger.Backends.ExceptionNotification do
-  use GenEvent
+  @behaviour :gen_event
+
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:configure, _options}, _state) do
+    {:ok, :ok, %{}}
+  end
 
   def handle_event({:error, _group_leader, {Logger, message, timestamp, metadata}}, state) do
     unless String.contains?(inspect(message), ["Fatal error: handshake failure",
