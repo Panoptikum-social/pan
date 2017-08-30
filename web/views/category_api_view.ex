a860ec3a815d2078857d821c2714ae89ae0fadd5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-08-31 00:07:26 +0200

    category tree properly displayed in jsonapi

--- web/views/category_api_view.ex
+++ web/views/category_api_view.ex
@@ -5,13 +5,16 @@ defmodule Pan.CategoryApiView do
   location "/categories/:id"
   attributes [:title]
 
-  has_one :parent,
-    serializer: Pan.CategoryApiView,
-    include: true,
-    field: :parent_id
+  has_one :parent,    serializer: Pan.PlainCategoryApiView, include: true
+  has_many :children, serializer: Pan.PlainCategoryApiView, include: true
+end
 
-  has_many :children,
-    serializer: Pan.CategoryApiView,
-    include: true,
-    field: :parent_id
+defmodule Pan.PlainCategoryApiView do
+  use Pan.Web, :view
+  use JaSerializer.PhoenixView
+
+  def type(_, _), do: "category"
+
+  location "/categories/:id"
+  attributes [:title]
 end
