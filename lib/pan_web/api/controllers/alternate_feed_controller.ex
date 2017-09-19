68c4dddab6e28073016af77d88cc1c5c4629ec2e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-09-19 10:41:36 +0200

    handle instance not found, experiment

--- lib/pan_web/api/controllers/alternate_feed_controller.ex
+++ lib/pan_web/api/controllers/alternate_feed_controller.ex
@@ -5,7 +5,7 @@ defmodule PanWeb.Api.AlternateFeedController do
 
 
   def show(conn, %{"id" => id}) do
-    alternate_feed = Repo.get(AlternateFeed, id)
+    alternate_feed = Repo.get!(AlternateFeed, id)
                      |> Repo.preload([:feed])
 
     render conn, "show.json-api", data: alternate_feed
