1a820023b0ddc2e537d68a9f55c12a83b1ccfdb3
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-09 08:59:25 +0100

    handle 521 web server down

--- lib/pan/parser/download.ex
+++ lib/pan/parser/download.ex
@@ -31,6 +31,7 @@ defmodule Pan.Parser.Download do
       {:ok, %HTTPoison.Response{status_code: 508}} -> {:error, "508: loop detected"}
       {:ok, %HTTPoison.Response{status_code: 509}} -> {:error, "509: Bandwidth Limit Exceeded"}
       {:ok, %HTTPoison.Response{status_code: 520}} -> {:error, "520: Unknown Error"}
+      {:ok, %HTTPoison.Response{status_code: 521}} -> {:error, "521: Web server is down"}
 
       {:ok, %HTTPoison.Response{status_code: 301, headers: headers}} -> redirect(url, headers)
       {:ok, %HTTPoison.Response{status_code: 302, headers: headers}} -> redirect(url, headers)
