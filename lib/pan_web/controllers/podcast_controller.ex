5d24c28dea34a9d57e6e554051ee2ef35590b782
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-14 08:12:59 +0100

    improve average counter

--- lib/pan_web/controllers/podcast_controller.ex
+++ lib/pan_web/controllers/podcast_controller.ex
@@ -63,7 +63,9 @@ defmodule PanWeb.PodcastController do
     retired = from(p in Podcast, where: p.retired == true)
               |> Repo.aggregate(:count, :id)
 
-    average = from(p in Podcast)
+    average = from(p in Podcast, where: p.next_update <= ^Timex.now() and
+                                        (is_nil(p.update_paused) or p.update_paused == false) and
+                                        (is_nil(p.retired) or p.retired == false)))
               |> Repo.aggregate(:avg, :update_intervall)
               |> Decimal.round(2)
 
