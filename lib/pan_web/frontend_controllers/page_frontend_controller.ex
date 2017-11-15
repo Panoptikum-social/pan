bf4b301e48573754ee007fec0d5c143caeb4748a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-15 09:10:28 +0100

    removed some con cache calls, only author button request stays in place

--- lib/pan_web/frontend_controllers/page_frontend_controller.ex
+++ lib/pan_web/frontend_controllers/page_frontend_controller.ex
@@ -4,25 +4,10 @@ defmodule PanWeb.PageFrontendController do
   alias PanWeb.Podcast
 
   def home(conn, _params) do
-    # popular_podcasts = ConCache.get_or_store(:slow_cache, :popular_podcasts, fn() ->
-    #                      (from p in Podcast, select: [p.subscriptions_count, p.id, p.title],
-    #                                          order_by: [desc: p.subscriptions_count],
-    #                                          limit: 10)
-    #                      |> Repo.all()
-    #                    end)
-
     popular_podcasts = (from p in Podcast, select: [p.subscriptions_count, p.id, p.title],
                                            order_by: [fragment("? DESC NULLS LAST", p.subscriptions_count)],
                                            limit: 10)
                        |> Repo.all()
-
-    # liked_podcasts = ConCache.get_or_store(:slow_cache, :liked_podcasts, fn() ->
-    #                      (from p in Podcast, select: [p.likes_count, p.id, p.title],
-    #                                          order_by: [desc: p.likes_count],
-    #                                          limit: 5)
-    #                      |> Repo.all()
-    #                    end)
-
 
     liked_podcasts = (from p in Podcast, select: [p.likes_count, p.id, p.title],
                                          order_by: [fragment("? DESC NULLS LAST", p.likes_count)],
