bbe78aca172c81f58b97f412ebe5cef7b7ad0e19
optikfluffel <optik@fluffel.io>
2017-11-16 13:26:56 +0100

    ase . syntax for accessing maps
    + remove some more credo warnings

--- lib/pan/parser/episode.ex
+++ lib/pan/parser/episode.ex
@@ -8,12 +8,12 @@ defmodule Pan.Parser.Episode do
   require Logger
 
   def get_or_insert(episode_map, podcast_id) do
-    case Repo.get_by(PanWeb.Episode, guid: episode_map[:guid], podcast_id: podcast_id) do
+    case Repo.get_by(PanWeb.Episode, guid: episode_map.guid, podcast_id: podcast_id) do
       nil ->
-        if episode_map[:guid] do
+        if episode_map.guid do
           insert(episode_map, podcast_id)
         else
-          case Repo.get_by(PanWeb.Episode, title: episode_map[:title] || episode_map[:subtitle],
+          case Repo.get_by(PanWeb.Episode, title: episode_map.title || episode_map.subtitle,
                                            podcast_id: podcast_id) do
             nil ->
               insert(episode_map, podcast_id)
@@ -29,7 +29,8 @@ defmodule Pan.Parser.Episode do
 
   def insert(episode_map, podcast_id) do
     # Here comes the line with the initial update time
-    Repo.get(PanWeb.Podcast, podcast_id)
+    PanWeb.Podcast
+    |> Repo.get(podcast_id)
     |> PanWeb.Podcast.changeset(%{update_intervall: 10,
                                next_update: Timex.shift(Timex.now(), hours: 10)})
     |> Repo.update()
@@ -41,9 +42,9 @@ defmodule Pan.Parser.Episode do
 
 
   def get(episode_map, podcast_id) do
-    case Repo.get_by(PanWeb.Episode, guid: episode_map[:guid], podcast_id: podcast_id) do
+    case Repo.get_by(PanWeb.Episode, guid: episode_map.guid, podcast_id: podcast_id) do
       nil ->
-        case Repo.get_by(PanWeb.Episode, title: episode_map[:title], podcast_id: podcast_id) do
+        case Repo.get_by(PanWeb.Episode, title: episode_map.title, podcast_id: podcast_id) do
           nil ->
             {:error, "not_found"}
           episode ->
@@ -58,29 +59,30 @@ defmodule Pan.Parser.Episode do
   def persist_many(episodes_map, podcast) do
     for {_, episode_map} <- episodes_map do
 
-      if episode_map[:enclosures] do
-        first_enclosure = episode_map[:enclosures] |> Map.to_list |> List.first |> elem(1)
-        fallback_url = if episode_map[:link], do: episode_map[:link], else: first_enclosure[:url]
+      if episode_map.enclosures do
+        first_enclosure = episode_map.enclosures |> Map.to_list |> List.first |> elem(1)
+        fallback_url = if episode_map.link, do: episode_map.link, else: first_enclosure.url
 
-        plain_episode_map = Map.drop(episode_map, [:chapters, :enclosures, :contributors])
-                            |> Map.put_new(:guid, fallback_url)
+        plain_episode_map = episode_map
+        |> Map.drop([:chapters, :enclosures, :contributors])
+        |> Map.put_new(:guid, fallback_url)
 
         case get_or_insert(plain_episode_map, podcast.id) do
           {:ok, episode} ->
-            if episode_map[:chapters] do
-              for {_, chapter_map} <- episode_map[:chapters] do
+            if episode_map.chapters do
+              for {_, chapter_map} <- episode_map.chapters do
                 Chapter.get_or_insert(chapter_map, episode.id)
               end
             end
 
-            if episode_map[:enclosures] do
-              for {_, enclosure_map} <- episode_map[:enclosures] do
+            if episode_map.enclosures do
+              for {_, enclosure_map} <- episode_map.enclosures do
                 Enclosure.get_or_insert(enclosure_map, episode.id)
               end
             end
 
-            Contributor.persist_many(episode_map[:contributors], episode)
-            Author.get_or_insert_persona_and_gig(episode_map[:author], episode, podcast)
+            Contributor.persist_many(episode_map.contributors, episode)
+            Author.get_or_insert_persona_and_gig(episode_map.author, episode, podcast)
             Logger.info "\n\e[33m === Importing new episode: #{episode.title} ===\e[0m"
           {:exists, _episode} ->
             true
@@ -92,16 +94,17 @@ defmodule Pan.Parser.Episode do
 
   def insert_contributors(episodes_map, podcast) do
     for {_, episode_map} <- episodes_map do
-      if episode_map[:enclosures] do
-        first_enclosure = episode_map[:enclosures] |> Map.to_list |> List.first |> elem(1)
-        fallback_url = if episode_map[:link], do: episode_map[:link], else: first_enclosure[:url]
+      if episode_map.enclosures do
+        first_enclosure = episode_map.enclosures |> Map.to_list |> List.first |> elem(1)
+        fallback_url = if episode_map.link, do: episode_map.link, else: first_enclosure.url
 
-        plain_episode_map = Map.drop(episode_map, [:chapters, :enclosures, :contributors])
-                            |> Map.put_new(:guid, fallback_url)
+        plain_episode_map = episode_map
+        |> Map.drop([:chapters, :enclosures, :contributors])
+        |> Map.put_new(:guid, fallback_url)
 
         case get(plain_episode_map, podcast.id) do
           {:exists, episode} ->
-            Contributor.persist_many(episode_map[:contributors], episode)
+            Contributor.persist_many(episode_map.contributors, episode)
             Logger.info "\n\e[33m === Updating contributors for episode: #{episode.title} ===\e[0m"
 
           {:error, "not_found"} ->
@@ -116,8 +119,11 @@ defmodule Pan.Parser.Episode do
     episodes = Repo.all(PanWeb.Episode)
 
     for episode <- episodes do
-      PanWeb.Episode.changeset(episode, %{description: HtmlSanitizeEx2.basic_html_reduced(episode.description),
-                                          summary:     HtmlSanitizeEx2.basic_html_reduced(episode.summary)})
+      episode
+      |> PanWeb.Episode.changeset(
+        %{description: HtmlSanitizeEx2.basic_html_reduced(episode.description),
+          summary:     HtmlSanitizeEx2.basic_html_reduced(episode.summary)}
+      )
       |> Repo.update()
     end
   end
