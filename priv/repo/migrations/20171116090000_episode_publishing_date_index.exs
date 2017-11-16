748bf7ac96132e32614e562dc8a4f0fc4b940b81
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-16 20:54:34 +0100

    epsiode publishing date index

---
+++ priv/repo/migrations/20171116090000_episode_publishing_date_index.exs
@@ -0,0 +1,7 @@
+defmodule Pan.Repo.Migrations.EpisodePublishingDateINDEX do
+  use Ecto.Migration
+
+  def change do
+    create index(:episodes, ["publishing_date ASC NULLS LAST", :podcast_id])
+  end
+end
