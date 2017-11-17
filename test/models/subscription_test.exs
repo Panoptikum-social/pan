9866d63ab0cc219839c411e6e3c4b3f646a4ec17
optikfluffel <optik@fluffel.io>
2017-11-17 12:04:41 +0100

    First baby steps towards better testing

--- test/models/subscription_test.exs
+++ test/models/subscription_test.exs
@@ -3,20 +3,39 @@ defmodule Pan.SubscriptionTest do
 
   alias PanWeb.Subscription
 
-  @valid_attrs %{}
-  @invalid_attrs %{}
+  describe "changeset" do
+    test "is valid with valid attributes" do
+      podcast = insert_podcast()
+      user = insert_user()
+      changeset = Subscription.changeset(%Subscription{}, %{user_id: user.id,
+                                                            podcast_id: podcast.id})
+      assert changeset.valid?
+    end
 
-  test "changeset with valid attributes" do
-    podcast = insert_podcast()
-    user = insert_user()
-    changeset = Subscription.changeset(%Subscription{},
-                                       Map.merge(@valid_attrs, %{user_id: user.id,
-                                                                 podcast_id: podcast.id}))
-    assert changeset.valid?
+    test "is invalid with empty attributes" do
+      changeset = Subscription.changeset(%Subscription{}, %{})
+      refute changeset.valid?
+    end
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Subscription.changeset(%Subscription{}, @invalid_attrs)
-    refute changeset.valid?
+
+  describe "get_or_insert" do
+    test "inserts new subscription" do
+      podcast = insert_podcast()
+      user = insert_user()
+
+      assert {:ok, subscription} = Subscription.get_or_insert(user.id, podcast.id)
+      id = subscription.id
+      assert %Subscription{id: ^id} = Repo.get!(Subscription, id)
+    end
+
+    test "gets existing subscription" do
+      podcast = insert_podcast()
+      user = insert_user()
+      {:ok, existing} = Subscription.get_or_insert(user.id, podcast.id)
+
+      assert {:ok, subscription} = Subscription.get_or_insert(user.id, podcast.id)
+      assert existing.id == subscription.id
+    end
   end
 end
