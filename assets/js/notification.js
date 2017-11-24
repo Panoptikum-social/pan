e1f7838a5dd522251112b13564e5d6127621a23b
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-24 09:13:02 +0100

    a bit more space between notifications

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -10,7 +10,7 @@ let Notification = {
     if(window.lastMessage != message){
       $.notify({message: message},
                {type: response.type,
-                spacing: 0,
+                spacing: 5,
                 newest_on_top: true})
       window.lastMessage = message
     }
