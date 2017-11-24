59fb0461ea503395ef1b000a5c30ae49c3817533
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-11-24 01:21:06 +0100

    episode notification, block in gui

--- lib/pan/parser/episode.ex
+++ lib/pan/parser/episode.ex
@@ -228,12 +228,16 @@ defmodule Pan.Parser.Episode do
 
 
   defp update_from_feed_one(%{enclosures: enclosures} = episode_map, podcast) do
-
-
     first_enclosure = unwrap_first_enclosure(enclosures)
     plain_episode_map = clean_episode(episode_map, first_enclosure.url)
 
     with {:ok, episode} <- insert_or_update(plain_episode_map, podcast.id) do
+      PanWeb.Endpoint.broadcast("podcasts:" <> Integer.to_string(podcast.id),
+                                "notification",
+                                %{content: "Updating episode #{episode.id} #{episode.title} ...",
+                                  type: "success"})
+
+
       get_or_insert_enclosures(enclosures, episode.id)
 
       if episode_map[:chapters] do
