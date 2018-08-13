80e0e766334f67193c78dff3f704569751a98241
Stefan Haslinger <stefan.haslinger@mittenin.at>
2018-08-13 16:00:48 +0200

    updated dependencies

--- mix.exs
+++ mix.exs
@@ -30,16 +30,16 @@ defmodule Pan.Mixfile do
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
-    [{:phoenix, "~> 1.3.3"}, # web framework
+    [{:phoenix, "~> 1.3.4"}, # web framework
      {:phoenix_pubsub, "~> 1.0"}, # PubSub messaging
      {:postgrex, "0.13.5"}, # database adapter
      {:phoenix_ecto, "3.3.0"}, # ORM (yes!)
-     {:phoenix_html, "2.11.2"}, # view layer
+     {:phoenix_html, "2.12.0"}, # view layer
      {:phoenix_live_reload, "1.1.5", only: :dev}, # live browser page reload on code changes
      {:gettext, "~> 0.9"}, # i18n and l10n
      {:cowboy, "1.1.2"}, # web server
      {:comeonin, "4.1.1"}, # password hashing library
-     {:bcrypt_elixir, "1.0.7"}, # algorithm used for comeonin
+     {:bcrypt_elixir, "1.0.9"}, # algorithm used for comeonin
      {:sweet_xml, "~> 0.6"}, # XML parser
      {:timex, "~> 3.3.0"}, # time conversion
      {:httpoison, "~> 1.2.0"}, # http client
@@ -50,18 +50,18 @@ defmodule Pan.Mixfile do
      {:html_sanitize_ex, "1.3.0"}, # sanitizing html input (shownotes)
      {:scrivener_ecto, "1.3.0"}, # pagination
      {:scrivener_html, "1.7.1"}, # pagination view helper
-     {:bamboo_smtp, "~> 1.5.0-rc.4"}, # mailing smtp adapter,
+     {:bamboo_smtp, "~> 1.5.0"}, # mailing smtp adapter,
      {:bamboo, "~> 1.0.0"}, # mailing
      {:earmark, "1.2.5"}, # Markdown parser
      {:timelier, "~> 0.9.2"}, # Cron like agent,
      {:tzdata, "~> 0.5.17"}, # Timezone information
      {:tirexs, "~> 0.8.15"}, # elasticsearch connector
-     {:credo, "0.9.3", only: [:dev, :test]}, # Code analysis
-     {:floki, "~> 0.20.2"}, # HTML parser
-     {:erlware_commons, "~> 1.1.0"},
+     {:credo, "0.10.0", only: [:dev, :test]}, # Code analysis
+     {:floki, "~> 0.20.3"}, # HTML parser
+     {:erlware_commons, "~> 1.2.0"},
      {:ja_serializer, git: "https://github.com/vt-elixir/ja_serializer"}, #Jsonapi.org serializer
-     {:p1_utils, "1.0.11", manager: :rebar}, # dependency for iconv
-     {:iconv, "~> 1.0.7", manager: :rebar}, # Unicode converter
+     {:p1_utils, "1.0.12", manager: :rebar}, # dependency for iconv
+     {:iconv, "~> 1.0.8", manager: :rebar}, # Unicode converter
      {:mogrify, "~> 0.6.1"}, # Imagemagick wrapper
      {:exactor, "~> 2.2.4", warn_missing: false} # Simplifies implementation of GenServer based processes
     ]
