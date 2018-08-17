ef9f9ebd00f904ff07301435369f6f1768985bff
Stefan Haslinger <stefan.haslinger@mittenin.at>
2018-08-17 15:08:15 +0200

    code formatting

--- lib/pan/updater/podcast.ex
+++ lib/pan/updater/podcast.ex
@@ -8,7 +8,8 @@ defmodule Pan.Updater.Podcast do
   def import_new_episodes(podcast, current_user \\ nil) do
     with {:ok, _podcast} <- set_next_update(podcast),
          {:ok, feed} <- Feed.get_by_podcast_id(podcast.id),
-         {:ok, map} <- RssFeed.import_to_map(feed.self_link_url, podcast.id, true), # do check for changes!
+         # do check for changes!
+         {:ok, map} <- RssFeed.import_to_map(feed.self_link_url, podcast.id, true),
          {:ok, _} <- Persistor.delta_import(map, podcast.id),
          {:ok, _} <- unpause_and_reset_failure_count(podcast) do
       notify_user(current_user, {:ok, "imported"}, podcast)
@@ -16,14 +17,17 @@ defmodule Pan.Updater.Podcast do
       {:redirect, redirect_target} ->
         Feed.update_with_redirect_target(podcast.id, H.to_255(redirect_target))
         import_new_episodes(podcast, current_user)
+
       {:error, :not_found} ->
         increase_failure_count(podcast)
         message = "=== Podcast #{podcast.id} has no feed! ==="
         Logger.error(message)
         notify_user(current_user, {:error, message}, podcast)
+
       {:error, message} ->
         increase_failure_count(podcast)
         notify_user(current_user, {:error, message}, podcast)
+
       {:done, "nothing to do"} ->
         nil
     end
@@ -40,6 +44,7 @@ defmodule Pan.Updater.Podcast do
   end
 
   defp notify_user(nil, _, _), do: nil
+
   defp notify_user(current_user, {status, message}, podcast) do
     notification = build_notification(podcast, {status, message}, current_user)
     Endpoint.broadcast("mailboxes:#{current_user.id}", "notification", notification)
@@ -67,17 +72,16 @@ defmodule Pan.Updater.Podcast do
 
   defp unpause_and_reset_failure_count(podcast) do
     PanWeb.Podcast.changeset(podcast, %{update_paused: false, failure_count: 0})
-    |> Repo.update([force: true])
+    |> Repo.update(force: true)
   end
-
 
   defp increase_failure_count(podcast) do
     Podcast.changeset(podcast, %{failure_count: (podcast.failure_count || 0) + 1})
-    |> Repo.update([force: true])
+    |> Repo.update(force: true)
 
     if podcast.failure_count == 9 do
       Podcast.changeset(podcast, %{retired: true})
-      |> Repo.update([force: true])
+      |> Repo.update(force: true)
     end
   end
 end
