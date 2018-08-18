fd9ae5e898d92d6709b068617fb3c4258d8fb286
Stefan Haslinger <stefan.haslinger@mittenin.at>
2018-08-18 07:47:40 +0200

    code formatting

--- lib/pan/updater/feed.ex
+++ lib/pan/updater/feed.ex
@@ -9,8 +9,7 @@ defmodule Pan.Updater.Feed do
       {:ok, "go on"}
     else
       headers = [
-        "User-Agent":
-          "Mozilla/5.0 (compatible; Panoptikum; +https://panoptikum.io/)"
+        "User-Agent": "Mozilla/5.0 (compatible; Panoptikum; +https://panoptikum.io/)"
       ]
 
       options = [recv_timeout: 15_000, timeout: 15_000, hackney: [:insecure]]
@@ -19,6 +18,7 @@ defmodule Pan.Updater.Feed do
         {:ok, %HTTPoison.Response{headers: headers}} ->
           headermap = Enum.into(headers, %{})
           check_headers(podcast, feed, headermap["ETag"], headermap["Last-Modified"])
+
         {:error, _error} ->
           {:ok, "go on"}
       end
@@ -28,6 +28,7 @@ defmodule Pan.Updater.Feed do
   def check_headers(_, feed, nil, nil) do
     Feed.changeset(feed, %{no_headers_available: true})
     |> Repo.update()
+
     {:ok, "go on"}
   end
 
@@ -57,7 +58,8 @@ defmodule Pan.Updater.Feed do
   def check_trustwortyness(feed, podcast, last_modified) do
     # We allow for a difference of (rather arbitrary) 100 seconds between last_modified header
     # and the last build date of the podcast or the publishing date of latest episode:
-    if (podcast.last_build_date && abs(Timex.diff(podcast.last_build_date, last_modified, :seconds)) < 100) ||
+    if (podcast.last_build_date &&
+          abs(Timex.diff(podcast.last_build_date, last_modified, :seconds)) < 100) ||
          abs(Timex.diff(podcast.latest_episode_publishing_date, last_modified, :seconds)) < 100 do
       Feed.changeset(feed, %{last_modified: last_modified, trust_last_modified: true})
       |> Repo.update()
