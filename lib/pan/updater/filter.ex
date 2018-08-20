6ba121eebfd86313e1882b79c78fd88f74097d21
Stefan Haslinger <stefan.haslinger@mittenin.at>
2018-08-20 17:12:07 +0200

    only 12 episodes have no guid -> let's ignore titles and rely on guids

--- lib/pan/updater/filter.ex
+++ lib/pan/updater/filter.ex
@@ -4,38 +4,28 @@ defmodule Pan.Updater.Filter do
   alias PanWeb.Episode
 
   def only_new_items(feed_map, podcast_id) do
-    existing_guids =
+    known_guids =
       from(e in Episode,
         where: e.podcast_id == ^podcast_id,
         select: e.guid
       )
       |> Repo.all()
 
-    existing_titles =
-      from(e in Episode,
-        where: e.podcast_id == ^podcast_id,
-        select: e.title
-      )
-      |> Repo.all()
-
     items =
       Quinn.find(feed_map, [:rss, :channel, :item])
-      |> Enum.reject(&is_already_imported?(&1, :guid, existing_guids))
-      |> Enum.reject(&is_already_imported?(&1, :title, existing_titles))
-      |> Enum.reject(&is_already_imported?(&1, :subtitle, existing_titles))
-
+      |> Enum.reject(&known_guid?(&1, known_guids))
     {:ok, items}
   end
 
-  defp is_already_imported?(item, attribute, values) do
-    case Enum.find(item[:value], &match?(%{name: ^attribute}, &1)) do
+  defp known_guid?(item, known_guids) do
+    case Enum.find(item[:value], &match?(%{name: :guid}, &1)) do
       nil ->
         false
 
       attribute_map ->
         Map.get(attribute_map, :value)
         |> List.first()
-        |> (&Enum.member?(values, &1)).()
+        |> (&Enum.member?(known_guids, &1)).()
     end
   end
 end
