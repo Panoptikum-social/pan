13881f3a7618b54d96de9396c38cc7e6d43c7616
stefan Haslinger <mail@stefan-haslinger.at>
2019-07-09 10:13:12 +0200

    create release config file

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pan.Mixfile do
 
   def project do
     [app: :pan,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
