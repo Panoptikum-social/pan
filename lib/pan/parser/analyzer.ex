06a6e1910adbc6be9e4cc405d42ed04793c77df2
stefan Haslinger <mail@stefan-haslinger.at>
2020-05-10 15:52:20 +0200

    parser

--- lib/pan/parser/analyzer.ex
+++ lib/pan/parser/analyzer.ex
@@ -262,7 +262,7 @@ defmodule Pan.Parser.Analyzer do
     :"media:comments", :author_picture, :"castbox:tid", :"castbox:episode_premium", :"omny:clipId",
     :"acast:settings", :imageSource, :"cba:teaser", :featuredthumbnail, :featuredImage, :licence,
     :toothashtag, :tootimage, :tootwith, :"podcastproduzenten:rellink", :"npo:mid", :"adori:feed",
-    :"rawvoice:isHd", :"podcastproduzenten:rellink"
+    :"rawvoice:isHd", :"podcastproduzenten:rellink", :customid
   ], do: %{}
 
   def call(_, "image", [tag_atom, _, _]) when tag_atom in [
