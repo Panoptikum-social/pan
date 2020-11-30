31e9eecf7e204fc5c91b2bdd5eede32e1f1f1647
stefan Haslinger <mail@stefan-haslinger.at>
2020-11-30 08:36:31 +0100

    typo in versions

--- priv/repo/migrations/20201130082400_update_thumbnailed_indices_v4.exs
+++ priv/repo/migrations/20201130082400_update_thumbnailed_indices_v4.exs
@@ -2,28 +2,28 @@ defmodule Pan.Repo.Migrations.UpdateThumbnailedIndices do
   use Ecto.Migration
 
   def change do
-    drop(index(:podcasts, [:id], name: :podcasts_not_thumbnailed_v2))
-    drop(index(:personas, [:id], name: :personas_not_thumbnailed_v2))
-    drop(index(:episodes, [:id], name: :episodes_not_thumbnailed_v2))
+    drop(index(:podcasts, [:id], name: :podcasts_not_thumbnailed_v3))
+    drop(index(:personas, [:id], name: :personas_not_thumbnailed_v3))
+    drop(index(:episodes, [:id], name: :episodes_not_thumbnailed_v3))
 
     create(
       index(:podcasts, [:id],
         where: "NOT thumbnailed AND NOT (image_url IS NULL)",
-        name: :podcasts_not_thumbnailed_v3
+        name: :podcasts_not_thumbnailed_v4
       )
     )
 
     create(
       index(:personas, [:id],
         where: "NOT thumbnailed AND NOT (image_url IS NULL)",
-        name: :personas_not_thumbnailed_v3
+        name: :personas_not_thumbnailed_v4
       )
     )
 
     create(
       index(:episodes, [:id],
         where: "NOT thumbnailed AND NOT (image_url IS NULL)",
-        name: :episodes_not_thumbnailed_v3
+        name: :episodes_not_thumbnailed_v4
       )
     )
   end
