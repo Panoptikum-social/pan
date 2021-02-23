8ceb018610768572b4b73ac7e304c65ac61fd41e
stefan Haslinger <mail@stefan-haslinger.at>
2021-02-23 13:32:14 +0100

    default font, tailwind-form plugin, footer styling

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,11 +3,30 @@ module.exports = {
     enabled: process.env.MIX_ENV === "prod",
     content: [
       "../lib/**/*.eex",
-      "../lib/**/*.leex"
+      "../lib/**/*.leex",
+      "../**/views/**/*.ex",
+      "../**/live/**/*.ex",
+      "./js/**/*.js",  
     ],
     options: {
       whitelist: []
     }
   },
-  plugins: [require("kutty")]
+  plugins: [
+    require("kutty"),
+    require('@tailwindcss/forms'),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Ubuntu'],
+      mono: ['Ubuntu\\ Mono'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['odd', 'even'],
+    },
+  },
 }
