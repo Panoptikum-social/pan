8f281b9a7cfb1542d0ab2d399681be9406708502
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-02 11:49:02 +0100

    columns and panels

--- lib/pan_web/surface/panel.ex
+++ lib/pan_web/surface/panel.ex
@@ -1,3 +1,18 @@
 defmodule PanWeb.Surface.Panel do
   use Surface.Component
+
+  prop heading, :string, required: true
+  prop purpose, :string, required: false, default: "default"
+  slot default
+
+  def heading_color_classes(purpose) do
+    case purpose do
+      "podcast" -> "bg-blue-400 text-white"
+      "popular" -> "bg-teal-500 text-white"
+      "like" -> "bg-rose-600 text-white"
+      "episode" -> "bg-amber-400 text-white"
+      "recommendation" -> "bg-lime-500 text-white"
+      _ -> "bg-white"
+    end
+  end
 end
