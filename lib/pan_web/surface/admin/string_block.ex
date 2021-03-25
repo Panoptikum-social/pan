740fc7478860ddfad0101f9abf307ecba2e99789
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-25 13:37:55 +0100

    bugfix: zebra stripes

--- lib/pan_web/surface/admin/string_block.ex
+++ lib/pan_web/surface/admin/string_block.ex
@@ -16,12 +16,12 @@ defmodule PanWeb.Surface.Admin.StringBlock do
     ~H"""
     <div class="my-8 grid grid-cols-5">
       <For each={{ {column, index} <- @columns |> Enum.with_index() }}>
-        <div class={{"px-2 text-gray-darker font-mono text-right col-span-1",
-                     "odd:bg-gray-lightest": Integer.is_even(index)}}>
+        <div class={{ "px-2 text-gray-darker font-mono text-right col-span-1",
+                      "bg-gray-lightest": Integer.is_even(index) }}>
           {{ titelize(column.field) }}
         </div>
-        <div class={{"px-2 col-span-4",
-                     "odd:bg-gray-lightest": Integer.is_even(index)}}>
+        <div class={{ "px-2 col-span-4",
+                      "bg-gray-lightest": Integer.is_even(index) }}>
            <ShowPresenter record={{ @record }}
                           field={{ column.field }}
                           type={{ column.type }} />
