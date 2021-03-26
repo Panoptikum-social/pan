0c0215feddd57690eaf20e3376e8c9c3cba53474
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-26 10:04:59 +0100

    remove toast config (I guess, it came from the boilerplate)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,9 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
+// Import the CSS so that webpack will load it and use the MiniCssExtractPlugin.
 import "../css/app.css"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
+// Configure module entry points in "webpack.config.js".
 // Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
 //     import socket from "./socket"
-//
 import "alpinejs"
 import "phoenix_html"
 import {Socket} from "phoenix"
