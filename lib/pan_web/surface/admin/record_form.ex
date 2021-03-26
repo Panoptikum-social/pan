2097e5e1d89a6fedc04e264cd2654a3799afaf4b
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-26 13:13:49 +0100

    elixir-ls-settings fixed

--- lib/pan_web/surface/admin/record_form.ex
+++ lib/pan_web/surface/admin/record_form.ex
@@ -28,43 +28,45 @@ defmodule PanWeb.Surface.Admin.RecordForm do
 
   def render(assigns) do
     ~H"""
-    <h2 class="text-2xl">
-      <span class="text-gray">
-        Edit <span class="font-semibold">{{ name(@record) }}</span>
-      </span> &nbsp; {{ @record.title }}
-    </h2>
+    <div>
+      <h2 class="text-2xl">
+        <span class="text-gray">
+          Edit <span class="font-semibold">{{ name(@record) }}</span>
+        </span> &nbsp; {{ @record.title }}
+      </h2>
 
-    <Form action={{ Function.capture(Routes, @path_helper, 2).(@socket, :create) }}
-          for={{ @changeset }}
-          opts={{ autocomplete: "off", class: "mt-8" }}>
+      <Form action={{ Function.capture(Routes, @path_helper, 2).(@socket, :create) }}
+            for={{ @changeset }}
+            opts={{ autocomplete: "off", class: "mt-8" }}>
 
-      <Field :if={{ @changeset.action }}
-              name="error"
-              class="alert alert-danger">
-        An error occured. Please check the errors below!
-      </Field>
+        <Field :if={{ @changeset.action }}
+                name="error"
+                class="alert alert-danger">
+          An error occured. Please check the errors below!
+        </Field>
 
-      <div class="flex space-x-8">
-        <div>
-          <CheckBoxField :for={{ column <- boolean_columns(assigns) }}
-                        name={{ column.field }}
-                        label={{ column.field }}/>
-        </div>
-        <div>
-          <NumberField :for={{ column <- number_columns(assigns) }}
-                        name={{ column.field }} />
-        </div>
-        <div>
-          <DateTimeField :for={{ column <- datetime_columns(assigns) }}
+        <div class="flex space-x-8">
+          <div>
+            <CheckBoxField :for={{ column <- boolean_columns(assigns) }}
+                          name={{ column.field }}
+                          label={{ column.field }}/>
+          </div>
+          <div>
+            <NumberField :for={{ column <- number_columns(assigns) }}
                           name={{ column.field }} />
+          </div>
+          <div>
+            <DateTimeField :for={{ column <- datetime_columns(assigns) }}
+                            name={{ column.field }} />
+          </div>
+          <div>
+            <TextField :for={{ column <- string_columns(assigns) }}
+                        name={{ column.field }} />
+          </div>
         </div>
-        <div>
-          <TextField :for={{ column <- string_columns(assigns) }}
-                      name={{ column.field }} />
-        </div>
-      </div>
-      <Submit label="Save" />
-    </Form>
+        <Submit label="Save" />
+      </Form>
+    </div>
     """
   end
 end
