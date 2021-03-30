5952832c437759756f711e93b9b30519b452aeff
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-30 12:43:07 +0200

    DateTimeSelect fully working

--- lib/pan_web/surface/admin/date_time_select.ex
+++ lib/pan_web/surface/admin/date_time_select.ex
@@ -1,6 +1,7 @@
 defmodule PanWeb.Surface.Admin.DateTimeSelect do
   use Surface.Component
   alias Surface.Components.Form
+  alias Surface.Components.Form.Input.InputContext
   import Phoenix.HTML.Form
 
   prop(name, :string, required: true)
@@ -8,8 +9,11 @@ defmodule PanWeb.Surface.Admin.DateTimeSelect do
   def render_builder(assigns, b) do
     ~H"""
     <div class="ml-3 px-4 py-0 rounded-none">
-      📅 {{ b.(:day, [class: "w-16"]) }} {{ b.(:month, []) }} {{ b.(:year, [class: "w-20"]) }}
-      🕒 {{ b.(:hour, [class: "w-16"]) }} : {{ b.(:minute, [class: "w-16"]) }}
+      📅 {{ b.(:day, [class: "w-16 px-2 py-0 rounded-none"]) }}
+          {{ b.(:month, [class: "px-2 py-0 rounded-none"]) }}
+          {{ b.(:year, [class: "w-20 px-2 py-0 rounded-none"]) }}
+      🕒 {{ b.(:hour, [class: "w-16 px-2 py-0 rounded-none"]) }} :
+          {{ b.(:minute, [class: "w-16 px-2 py-0 rounded-none"]) }}
     </div>
     """
   end
@@ -18,9 +22,9 @@ defmodule PanWeb.Surface.Admin.DateTimeSelect do
     ~H"""
     <Form.Field name={{ @name |> String.to_atom() }} class="my-2 flex items-center justify-end">
       <Form.Label class="italic"/>
-      <Context get={{ form: form, field: field }}>
+      <InputContext assigns={{ assigns }} :let={{ form: form, field: field }}>
         {{ datetime_select(form, field, builder: fn b -> render_builder(assigns, b) end) }}
-      </Context>
+      </InputContext>
       <Form.ErrorTag />
     </Form.Field>
     """
