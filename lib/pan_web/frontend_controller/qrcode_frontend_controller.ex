706585cf7ea7303736aea16f6b31617de106c6cc
Stefan Haslinger <stefan.haslinger@mittenin.at>
2021-05-04 14:12:52 +0200

    qr-code showing up properly

--- lib/pan_web/frontend_controller/qrcode_frontend_controller.ex
+++ lib/pan_web/frontend_controller/qrcode_frontend_controller.ex
@@ -5,7 +5,7 @@ defmodule PanWeb.QRCodeFrontendController do
     svg =
       code
       |> EQRCode.encode()
-      |> EQRCode.svg()
+      |> EQRCode.svg(width: 150)
 
     conn
     |> put_resp_content_type("image/svg+xml")
