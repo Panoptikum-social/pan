c73cbca2f454706353afe6c6a78f5956f591c338
stefan Haslinger <mail@stefan-haslinger.at>
2021-05-13 11:01:07 +0200

    refactor full text search and implement episodes and podcasts (with bugs)

--- lib/pan/search.ex
+++ lib/pan/search.ex
@@ -1,124 +1,52 @@
 defmodule Pan.Search do
+  alias Pan.Search
   import Ecto.Query, only: [from: 2]
   alias Pan.Repo
-  alias PanWeb.{Category, Episode, Persona, Podcast}
   require Logger
   alias HTTPoison.Response
 
   def push_missing do
-    category_ids =
-      from(c in Category, where: not c.full_text, select: c.id)
+    Search.Category.batch_index()
+    Search.Persona.batch_index()
+    Search.Podcast.batch_index()
+    Search.Episode.batch_index()
+  end
+
+  def reset_all do
+    Search.Category.batch_reset()
+    Search.Podcast.batch_reset()
+    Search.Persona.batch_reset()
+    Search.Episode.batch_reset()
+  end
+
+  def batch_index(%{model: model, preloads: preloads, selects: selects}) do
+    record_ids =
+      from(r in model, where: not r.full_text, limit: 1_000, select: r.id)
       |> Repo.all()
 
     manticore_data =
-      from(c in Category, where: c.id in ^category_ids)
+      from(r in model, where: r.id in ^record_ids, preload: ^preloads, select: ^selects)
       |> Repo.all()
-      |> Enum.map(&%{insert: %{index: "categories", id: &1.id, doc: %{title: &1.title}}})
+      |> Enum.map(&model.manticore_struct(&1))
       |> Enum.map(&Jason.encode!(&1))
       |> Enum.join("\n")
 
-    {:ok, %Response{status_code: response_code, body: _response_body}} =
+    {:ok, %Response{status_code: response_code, body: response_body}} =
       HTTPoison.post("http://localhost:9308/bulk", manticore_data, [
         {"Content-Type", "application/x-ndjson"}
       ])
 
     if response_code == 200 do
-      from(c in Category, where: c.id in ^category_ids)
+      from(r in model, where: r.id in ^record_ids)
       |> Repo.update_all(set: [full_text: true])
 
-      Logger.info("=== Indexed #{length(category_ids)} categories ===")
+      Logger.info("=== Indexed #{length(record_ids)} records of type #{model} ===")
+    else
+      IO.inspect(response_body)
     end
 
-    Persona.batch_index()
-
-    # podcast_ids =
-    #   from(c in Podcast,
-    #     where: not c.full_text,
-    #     limit: 100,
-    #     select: c.id
-    #   )
-    #   |> Repo.all()
-
-    # for id <- podcast_ids, do: Podcast.update_search_index(id)
-
-    # from(c in Podcast, where: c.id in ^podcast_ids)
-    # |> Repo.update_all(set: [full_text: true])
-
-    # Logger.info("=== Indexed #{length(podcast_ids)} podcasts ===")
-
-    # episode_ids =
-    #   from(c in Episode,
-    #     where: not c.full_text,
-    #     limit: 1000,
-    #     select: c.id
-    #   )
-    #   |> Repo.all()
-
-    # for id <- episode_ids, do: Episode.update_search_index(id)
-
-    # from(c in Episode, where: c.id in ^episode_ids)
-    # |> Repo.update_all(set: [full_text: true])
-
-    # Logger.info("=== Indexed #{length(episode_ids)} episodes ===")
-    # Logger.info("=== Indexing finished ===")
-  end
-
-  def reset_all do
-    IO.puts("resetting all categories")
-    Repo.update_all(Category, set: [full_text: false])
-    reset_podcasts()
-    reset_personas()
-    reset_episodes()
-  end
-
-  defp reset_podcasts do
-    IO.puts("resetting up to 10_000 podcasts")
-
-    podcast_ids =
-      from(p in Podcast,
-        where: p.full_text == true,
-        select: p.id,
-        limit: 10_000
-      )
-      |> Repo.all()
-
-    from(p in Podcast, where: p.id in ^podcast_ids)
-    |> Repo.update_all(set: [full_text: false])
-
-    if length(podcast_ids) > 0, do: reset_podcasts()
-  end
-
-  defp reset_personas do
-    IO.puts("resetting up to 10_000 personas")
-
-    persona_ids =
-      from(p in Persona,
-        where: p.full_text == true,
-        select: p.id,
-        limit: 10_000
-      )
-      |> Repo.all()
-
-    from(p in Persona, where: p.id in ^persona_ids)
-    |> Repo.update_all(set: [full_text: false])
-
-    if length(persona_ids) > 0, do: reset_personas()
-  end
-
-  defp reset_episodes do
-    IO.puts("resetting up to 10_000 episodes")
-
-    episode_ids =
-      from(e in Episode,
-        where: e.full_text == true,
-        select: e.id,
-        limit: 10_000
-      )
-      |> Repo.all()
-
-    from(e in Episode, where: e.id in ^episode_ids)
-    |> Repo.update_all(set: [full_text: false])
-
-    if length(episode_ids) > 0, do: reset_episodes()
+    if response_code == 200 && length(record_ids) > 0 do
+      batch_index(%{model: model, preloads: preloads, selects: selects})
+    end
   end
 end
---
+++ lib/pan/search/persona.ex
@@ -0,0 +1,70 @@
+defmodule Pan.Search.Persona do
+  alias PanWeb.Persona
+  import Ecto.Query, only: [from: 2]
+  alias Pan.Repo
+  require Logger
+
+  def batch_index() do
+    Pan.Search.batch_index(
+      model: Persona,
+      preloads: [:episodes, :podcasts, :thumbnails],
+      selects: [
+        :id,
+        :name,
+        :pid,
+        :uri,
+        :description,
+        :long_description,
+        :image_title,
+        podcasts: :id,
+        episodes: :id,
+        thumbnails: [:path, :filename]
+      ]
+    )
+  end
+
+  def manticore_struct(persona) do
+    %{
+      insert: %{
+        index: "personas",
+        id: persona.id,
+        doc: %{
+          name: persona.name || "",
+          pid: persona.pid || "",
+          uri: persona.uri || "",
+          description: persona.description || "",
+          long_description: persona.long_description || "",
+          thumbnail_url: thumbnail_url(persona),
+          image_title: persona.image_title || "",
+          podcast_ids: Enum.map(persona.podcasts, & &1.id),
+          episode_ids: Enum.map(persona.episodes, & &1.id)
+        }
+      }
+    }
+  end
+
+  defp thumbnail_url(image) do
+    if length(image.thumbnails) > 0 do
+      hd(image.thumbnails).path <> hd(image.thumbnails).filename
+    else
+      ""
+    end
+  end
+
+  def batch_reset do
+    Logger.info("=== full_text reset up to 10_000 personas ===")
+
+    persona_ids =
+      from(p in Persona,
+        where: p.full_text == true,
+        select: p.id,
+        limit: 10_000
+      )
+      |> Repo.all()
+
+    from(p in Persona, where: p.id in ^persona_ids)
+    |> Repo.update_all(set: [full_text: false])
+
+    if length(persona_ids) > 0, do: batch_reset()
+  end
+end
