619d0f4753e1c560da36a5731d7a3f9f375ac05d
Stefan Haslinger <mail@stefan-haslinger.at>
2021-12-15 13:07:02 +0100

    replace character counter with plain js

--- lib/pan_web/live/podcast/recommend_form.ex
+++ lib/pan_web/live/podcast/recommend_form.ex
@@ -10,28 +10,27 @@ defmodule PanWeb.Live.Podcast.RecommendForm do
   prop(changeset, :map, required: true)
   prop(podcast, :map, required: true)
 
-
   def render(assigns) do
-  ~F"""
-    {#if @current_user_id}
-      <Form for={@changeset}
-            class="m-4"
-            action={recommendation_frontend_path(Endpoint, :create)}>
-        <TextInput field={:comment}
-                   opts={size: 100, maxlength: 255, placeholder: "Your recommendation"} />
-          <p class="help-block text-muted"><span id='remaining'>255</span> characters left</p>
-        <HiddenInput field={:podcast_id } value={@podcast.id} />
-        <Submit label={"Recommend"} />
-      </Form>
-    {/if}
+    ~F"""
+      {#if @current_user_id}
+        <Form for={@changeset}
+              class="m-4"
+              action={recommendation_frontend_path(Endpoint, :create)}>
+          <TextInput field={:comment}
+                     opts={size: 100, maxlength: 255, placeholder: "Your recommendation"} />
+            <p class="help-block text-muted"><span id='remaining'>255</span> characters left</p>
+          <HiddenInput field={:podcast_id } value={@podcast.id} />
+          <Submit label={"Recommend"} />
+        </Form>
+      {/if}
 
-    <script>
-      $('#recommendation_comment').keyup(function(){
-          $("#remaining").html((255 - this.value.length));
-      })
-    </script>
+      <script>
+        document.getElementById('recommendation_comment').onkeyup = function(){
+            document.getElementById("remaining").innerHTML = 255 - this.value.length;
+        }
+      </script>
 
-    <br/>
-  """
+      <br/>
+    """
   end
 end
