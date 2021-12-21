323be0c334b035e40aa244b3c81fa046d66ba6c7
Stefan Haslinger <stefan.haslinger@mittenin.at>
2021-12-21 12:23:13 +0100

    tree component refactored to single component with recursive function call

--- lib/pan_web/surface/tree.ex
+++ lib/pan_web/surface/tree.ex
@@ -1,24 +1,58 @@
 defmodule PanWeb.Surface.Tree do
   use Surface.LiveComponent
-  alias PanWeb.Surface.Node
+  alias PanWeb.Surface.{Icon, Tree}
 
-  prop nodes, :list, required: true
-  prop class, :css_class
-  prop indentation_level, :integer, required: false, default: 0
-  prop select, :event
-  prop selected_id, :integer
+  prop(nodes, :list, required: true)
+  prop(class, :css_class)
+  prop(select, :event)
+  prop(selected_id, :integer)
+  data(expanded, :map, default: %{})
+
+  def handle_event("toggle-expand", %{"node-id" => node_id}, socket) do
+    expanded = socket.assigns.expanded
+    node_id = node_id |> String.to_integer()
+
+    expanded =
+      if Map.has_key?(expanded, node_id) do
+        Map.delete(expanded, node_id)
+      else
+        Map.put(expanded, node_id, true)
+      end
+
+    {:noreply, assign(socket, :expanded, expanded)}
+  end
 
   def render(assigns) do
     ~F"""
     <div class={"flex flex-col", @class}>
-      {#for node <- @nodes}
-        <Node id={"node-#{node.id}"}
-              select={@select}
-              selected_id={@selected_id}
-              node={node}
-              indentation_level={@indentation_level}/>
-      {/for}
+      {render_tree(assigns, @nodes, 0)}
     </div>
+    """
+  end
+
+  defp render_tree(assigns, nodes, indentation_level) do
+    ~F"""
+    {#for node <- nodes}
+      <div :on-click={@select}
+           phx-value-node-id={node.id}
+           class={"px-2 py-0.5 border border-gray-light",
+                  "bg-success": @selected_id == node.id}>
+        <span class="font-mono text-gray-light">
+          {String.duplicate("&nbsp;", indentation_level) |> raw}
+        </span>
+
+        <button :if={node.children |> is_list && length(node.children) > 0}
+                :on-click="toggle-expand" phx-value-node-id={node.id}>
+          <Icon :if={@expanded[node.id]}  name="folder-open-heroicons-outline" />
+          <Icon :if={!@expanded[node.id]} name="folder-heroicons-outline" />
+        </button>
+
+        {node.title}
+      </div>
+      {#if @expanded[node.id] && is_list(node.children)}
+        {render_tree(assigns, node.children, indentation_level + 1)}
+      {/if}
+    {/for}
     """
   end
 end
