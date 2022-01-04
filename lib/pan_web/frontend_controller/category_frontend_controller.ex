bbcc9791cc578014b0c070e7dc4158c90a54cc3d
Stefan Haslinger <mail@stefan-haslinger.at>
2022-01-04 12:24:17 +0100

    remove _alt remainders

--- lib/pan_web/frontend_controller/category_frontend_controller.ex
+++ lib/pan_web/frontend_controller/category_frontend_controller.ex
@@ -29,56 +29,7 @@ defmodule PanWeb.CategoryFrontendController do
     end
   end
 
-  def show_alt(conn, %{"id" => id}) do
-    if category = Repo.get(Category, id) do
-      category =
-        category
-        |> Repo.preload(children: from(c in Category, order_by: c.title))
-        |> Repo.preload(:parent)
-
-      podcasts =
-        from(l in Language,
-          right_join: p in assoc(l, :podcasts),
-          join: c in assoc(p, :categories),
-          where: c.id == ^id,
-          select: %{id: p.id, title: p.title, language_name: l.name, language_emoji: l.emoji}
-        )
-        |> Repo.all()
-
-      render(conn, "show_alt.html",
-        category: category,
-        podcasts: podcasts
-      )
-    else
-      conn
-      |> put_status(:not_found)
-      |> render("not_found.html")
-    end
-  end
-
   def show_stats(conn, %{"id" => id}) do
-    category =
-      Category
-      |> Repo.get!(id)
-      |> Repo.preload(children: from(c in Category, order_by: c.title))
-      |> Repo.preload(:parent)
-
-    podcasts =
-      from(l in Language,
-        right_join: p in assoc(l, :podcasts),
-        join: c in assoc(p, :categories),
-        where: c.id == ^id,
-        select: %{id: p.id, title: p.title, language_name: l.name, language_emoji: l.emoji}
-      )
-      |> Repo.all()
-
-    render(conn, "show_stats.html",
-      category: category,
-      podcasts: podcasts
-    )
-  end
-
-  def show_stats_alt(conn, %{"id" => id}) do
     category =
       Category
       |> Repo.get!(id)
@@ -128,42 +79,6 @@ defmodule PanWeb.CategoryFrontendController do
         |> Repo.preload(podcasts: [:categories, [engagements: :persona]])
 
       render(conn, "categorized.html",
-        categories: categories,
-        category: category
-      )
-    else
-      render(conn, "no_community.html")
-    end
-  end
-
-  def categorized_alt(conn, %{"id" => id}) do
-    category =
-      Category
-      |> Repo.get!(id)
-      |> Repo.preload(:parent, podcasts: :categories)
-
-    if category.parent.title == "👩 👨 Community" do
-      podcast_ids =
-        from(cp in PanWeb.CategoryPodcast,
-          where: cp.category_id == ^id,
-          select: cp.podcast_id
-        )
-        |> Repo.all()
-
-      categories =
-        from(c in Category,
-          join: p in assoc(c, :podcasts),
-          where:
-            p.id in ^podcast_ids and
-              c.id != ^id,
-          order_by: c.title,
-          group_by: c.id
-        )
-        |> Repo.all()
-        |> Repo.preload(podcasts: from(p in Podcast, where: p.id in ^podcast_ids))
-        |> Repo.preload(podcasts: [:categories, [engagements: :persona]])
-
-      render(conn, "categorized_alt.html",
         categories: categories,
         category: category
       )
