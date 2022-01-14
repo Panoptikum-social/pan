9cffc561fa71e14b5dadee15d8e79ea1a70dc8c7
Stefan Haslinger <mail@stefan-haslinger.at>
2022-01-14 15:58:37 +0100

    admin/pages/index note used any more, it seems

--- lib/pan_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PanWeb.PageController do
-  use PanWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/pan_web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule PanWeb.PageView do
-  use PanWeb, :view
-end
