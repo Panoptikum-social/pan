3807f7b42800bfa543e24705984c7bb02b62ef1f
Stefan Haslinger <mail@stefan-haslinger.at>
2022-01-20 22:03:52 +0100

    bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pan.MixProject do
     [
       app: :pan,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers()  ++ [:surface],
       start_permanent: Mix.env() == :prod,
