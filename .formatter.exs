b7d90536da5e25609001e9b016c15915bb0c88d0
Stefan Haslinger <mail@stefan-haslinger.at>
2022-01-31 15:32:32 +0100

    fix form_for parens, prevent in future via .formatter.exs config

--- .formatter.exs
+++ .formatter.exs
@@ -3,5 +3,6 @@
   import_deps: [:ecto_sql, :phoenix],
   inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
   subdirectories: ["priv/*/migrations"],
-  surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"]
+  surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"],
+  locals_without_parens: [form_for: 3, form_for: 4]
 ]
