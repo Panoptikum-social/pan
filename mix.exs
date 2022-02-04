87ae199843b98983c9546436cbdb7463bfd5cc3e
Stefan Haslinger <mail@stefan-haslinger.at>
2022-02-04 15:23:45 +0100

    bump version number to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pan.MixProject do
   def project do
     [
       app: :pan,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers() ++ [:surface],
