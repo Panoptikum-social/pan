05220035a7c3006e1a463d224e1f06999b6e0dc7
Stefan Haslinger <mail@stefan-haslinger.at>
2022-02-07 13:14:45 +0100

    subscribe button with image, if available

--- lib/pan_web/live/podcast/subscribe.ex
+++ lib/pan_web/live/podcast/subscribe.ex
@@ -1,17 +1,29 @@
 defmodule PanWeb.Live.Podcast.Subscribe do
   use Surface.LiveView
-  alias PanWeb.Podcast
+  alias PanWeb.{Podcast, Image}
   alias PanWeb.Live.Podcast.PodloveSubscribeButton
 
   def mount(%{"id" => id}, _session, socket) do
-    {:ok, assign(socket, podcast: Podcast.get_by_id_with_feeds(id))}
+    {:ok,
+     assign(socket,
+       podcast: Podcast.get_by_id_with_feeds(id),
+       podcast_thumbnail: Image.get_by_podcast_id(id) || %{}
+     )}
   end
 
   def render(assigns) do
     ~F"""
-    <PodloveSubscribeButton id="podlove_subscribe_button"
-                            {=@podcast}
-                            class="grid place-items-center h-screen" />
+      <div class="flex flex-col h-screen space-y-4 justify-center items-center w-screen">
+          <img :if={Map.has_key?(@podcast_thumbnail, :path)}
+              src={"https://panoptikum.io#{@podcast_thumbnail.path}#{@podcast_thumbnail.filename}"}
+              width="150"
+              height="150"
+              alt={@podcast.image_title}
+              id="photo"
+              class="break-words text-xs" />
+          <PodloveSubscribeButton id="podlove_subscribe_button"
+                                  {=@podcast} />
+      </div>
     """
   end
 end
