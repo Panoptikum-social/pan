532d9da539530cf6c4cb77062f9d9dc9e4815d8f
Stefan Haslinger <mail@stefan-haslinger.at>
2022-02-08 21:05:11 +0100

    replace nprogress with topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,38 +4,55 @@
 // Configure module entry points in "webpack.config.js".
 // Import deps with the dep name or local files with a relative path, for example:
 //     import socket from "./socket"
-import "alpinejs"
-import "phoenix_html"
-import {Socket} from "phoenix"
-import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
-import {InfiniteScroll} from "./infinite_scroll"
-import {Notification} from "./notification"
-import Hooks from "./_hooks"
+import "alpinejs";
+import "phoenix_html";
+import { Socket } from "phoenix";
+import topbar from "topbar";
+import { LiveSocket } from "phoenix_live_view";
+import { InfiniteScroll } from "./infinite_scroll";
+import { Notification } from "./notification";
+import Hooks from "./_hooks";
 
-Hooks.InfiniteScroll = InfiniteScroll
-Hooks.Notification = Notification
+Hooks.InfiniteScroll = InfiniteScroll;
+Hooks.Notification = Notification;
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
   dom: {
-    onBeforeElUpdated(from, to){
-      if(from.__x){ window.Alpine.clone(from.__x, to) }
-    }
+    onBeforeElUpdated(from, to) {
+      if (from.__x) {
+        window.Alpine.clone(from.__x, to);
+      }
+    },
   },
-  params: {_csrf_token: csrfToken},
-  hooks: Hooks
-})
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+// Show progress bar on live navigation and form submits
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+let topBarScheduled = undefined;
+
+window.addEventListener("phx:page-loading-start", () => {
+  if (!topBarScheduled) {
+    topBarScheduled = setTimeout(() => topbar.show(), 400);
+  }
+});
+
+window.addEventListener("phx:page-loading-stop", () => {
+  clearTimeout(topBarScheduled);
+  topBarScheduled = undefined;
+  topbar.hide();
+});
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
