e358a9cd4b3c987ad531b0e8154f69743aa648d4
Stefan Haslinger <mail@stefan-haslinger.at>
2022-02-14 16:52:00 +0100

    show only one random recommendation

--- lib/pan_web/live/recommendation/random.ex
+++ lib/pan_web/live/recommendation/random.ex
@@ -4,27 +4,40 @@ defmodule PanWeb.Live.Recommendation.Random do
   alias PanWeb.{Podcast, Category}
 
   def mount(_params, _session, socket) do
-    podcast = Podcast.random()
-    category = List.first(podcast.categories).id |> Category.get_by_id()
-    episode = Enum.random(podcast.episodes)
+    if connected?(socket) do
+      podcast = Podcast.random()
+      category = List.first(podcast.categories).id |> Category.get_by_id()
+      episode = Enum.random(podcast.episodes)
 
-    {:ok,
-     assign(socket,
-       podcast: podcast,
-       episode: episode,
-       category: category,
-       page_title: "Random Recommendation"
-     )}
+      {:ok,
+       assign(socket,
+         podcast: podcast,
+         episode: episode,
+         category: category,
+         page_title: "Random Recommendation"
+       )}
+    else
+      {:ok, assign(socket, page_title: "Random Recommendation")}
+    end
   end
 
   def render(assigns) do
-    ~F"""
-    <h1 class="text-3xl">A Random Recommendation</h1>
-    <p class="mt-4 leading-9">
-      Fortuna opted for the episode &nbsp; <EpisodeButton for={@episode} /> &nbsp;
-      from the podcast  &nbsp; <PodcastButton for={@podcast} /> &nbsp;
-      in the category  &nbsp;<CategoryButton for={@category} /> &nbsp;.
-    </p>
-    """
+    if Map.has_key?(assigns, :podcast) do
+      ~F"""
+      <h1 class="text-3xl">A Random Recommendation</h1>
+      <p class="mt-4 leading-9">
+        Fortuna opted for the episode &nbsp; <EpisodeButton for={@episode} /> &nbsp;
+        from the podcast  &nbsp; <PodcastButton for={@podcast} /> &nbsp;
+        in the category  &nbsp;<CategoryButton for={@category} /> &nbsp;.
+      </p>
+      """
+    else
+      ~F"""
+      <h1 class="text-3xl">A Random Recommendation</h1>
+      <p class="mt-4 leading-9">
+        loading ...
+      </p>
+      """
+    end
   end
 end
