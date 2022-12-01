581f3fb52b88822878683c35c2429ad326629c95
Stefan Haslinger <mail@stefan-haslinger.at>
2022-12-01 09:29:26 +0100

    populate wisspod

--- lib/pan_web/router.ex
+++ lib/pan_web/router.ex
@@ -430,5 +430,6 @@ defmodule PanWeb.Router do
     get("/maintenance/stats", MaintenanceController, :stats)
     get("/maintenance/catch_up_thumbnailed", MaintenanceController, :catch_up_thumbnailed)
     get("/maintenance/exception_notification", MaintenanceController, :exception_notification)
+    get("/maintenance/populate-wisspod", MaintenanceController, :populate_wisspod)
   end
 end
