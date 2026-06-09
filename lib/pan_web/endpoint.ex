6032368bac098e833dd35e6aec2b8be87bbabe5b
Stefan Haslinger <stefan.haslinger@informatom.com>
2026-06-09 17:34:02 +0200

    disable websocket logging

--- lib/pan_web/endpoint.ex
+++ lib/pan_web/endpoint.ex
@@ -12,7 +12,8 @@ defmodule PanWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [
-      connect_info: [session: @session_options]
+      connect_info: [session: @session_options],
+      log: false
     ]
 
   # Serve at "/" the static files from "priv/static" directory.
